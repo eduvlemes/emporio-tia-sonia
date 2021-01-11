d0f9dd5d83633c19a60e50928530150dee630fd9
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-01-11 15:40:23 -0300

    chore: update layout

---
+++ storefront.webpack.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './js/ShippingLine.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingLine.js')
+    }
+  }
+})
