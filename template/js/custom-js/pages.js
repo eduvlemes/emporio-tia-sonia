f236b80de44291029a15026264ada45717fecd7f
eduvlemes <edu.vlemes@gmail.com>
2021-02-11 19:39:59 -0300

    ajustes wesley

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
+$('.header__search-input').keyup(function(){
+    $('body .search__input').val($(this).val()).[0].dispatchEvent(new Event('input'));
+});
+
+$('body').click(function(e){
+    if($(e.target).closest('.header__search').length == 0){
+        $('#instant-search .search__status .close').click();
+    }
+});
