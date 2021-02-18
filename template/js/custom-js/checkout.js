91d2a0bf1d5d9970674a4f6c4957c95450db169f
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-02-18 12:02:29 -0300

    chore: update acompanhar pedido

--- template/js/custom-js/checkout.js
+++ template/js/custom-js/checkout.js
@@ -1,1 +1,12 @@
 // Add your custom JavaScript for checkout here.
+window.storefront.on('widget:@ecomplus/widget-fb-pixel', function () {
+  setTimeout(function () {
+    const pedido = document.getElementById('order') || false
+    const statusOrderPaid = document.querySelector('.order-info__financial-status--paid') || false
+    if (pedido && statusOrderPaid) {
+      const timeShipping = document.querySelector('.shipping-line')
+      const htmlLink = '<br><a href="/acompanhar-pedido" target="blank">Acompanhar Pedido</a>'
+      timeShipping.insertAdjacentHTML('afterend', htmlLink)
+    }
+  }, 800)
+})
